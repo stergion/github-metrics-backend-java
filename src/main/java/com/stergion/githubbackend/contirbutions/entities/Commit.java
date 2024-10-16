772c740cca0fcb2d6eeaad0ef9c69f54a71acbe1
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2024-10-16 21:03:48 +0300

    Override equals(), hashCode(), toString methods on Commit.java

--- src/main/java/com/stergion/githubbackend/contirbutions/entities/Commit.java
+++ src/main/java/com/stergion/githubbackend/contirbutions/entities/Commit.java
@@ -26,4 +26,37 @@ public non-sealed class Commit extends Contribution {
     public List<File> files;
     public int filesCount;
 
+    @Override
+    public String toString() {
+        return  "{ id: " + id +
+                ", userId: " + userId +
+                ", repositoryId: " + repositoryId +
+                ", github: " + github +
+                "committedDate: " + committedDate +
+                ", pushedDate: " + pushedDate +
+                ", additions: " + additions +
+                ", deletions: " + deletions +
+                ", comments: " + comments +
+                ", commentsCount: " + commentsCount +
+                ", associatedPullRequest: " + associatedPullRequest +
+                ", associatedPullRequestsCount: " + associatedPullRequestsCount +
+                ", files: " + files +
+                ", filesCount: " + filesCount +
+                "}";
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        return (o instanceof Commit c)
+                && userId.equals(c.userId)
+                && repositoryId.equals(c.repositoryId);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = userId.hashCode();
+        result = 31 * result + repositoryId.hashCode();
+        return result;
+    }
+
 }
