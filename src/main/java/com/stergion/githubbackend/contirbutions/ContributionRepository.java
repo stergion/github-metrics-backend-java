d0392ffadb4026ccc01200806beb08d4b345f378
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2024-11-27 18:24:59 +0200

    fix: Remove unnecessary contribution Repository

--- src/main/java/com/stergion/githubbackend/contirbutions/ContributionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.stergion.githubbackend.contirbutions;
-
-import com.stergion.githubbackend.contirbutions.entities.Contribution;
-import io.quarkus.mongodb.panache.PanacheMongoRepository;
-
-public class ContributionRepository implements PanacheMongoRepository<Contribution> {
-}
