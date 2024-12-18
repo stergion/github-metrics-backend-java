9a923cfe2f9395d4d2773092ab964453deae54be
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2024-12-18 17:33:55 +0200

    feat: Add ServiceResponseExceptionMapper for error handling

--- src/main/java/com/stergion/githubbackend/external/githubservice/client/GitHubServiceClient.java
+++ src/main/java/com/stergion/githubbackend/external/githubservice/client/GitHubServiceClient.java
@@ -1,9 +1,11 @@
 package com.stergion.githubbackend.external.githubservice.client;
 
+import com.stergion.githubbackend.external.githubservice.client.exceptions.ServiceResponseExceptionMapper;
 import com.stergion.githubbackend.external.githubservice.client.routes.ContributionRoutes;
 import com.stergion.githubbackend.external.githubservice.client.routes.RepositoryRoutes;
 import com.stergion.githubbackend.external.githubservice.client.routes.UserRoutes;
 import jakarta.ws.rs.Path;
+import org.eclipse.microprofile.rest.client.annotation.RegisterProvider;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 /**
@@ -14,7 +16,9 @@ import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
  *
  * @see RegisterRestClient
  */
+
 @Path("/api")
+@RegisterProvider(ServiceResponseExceptionMapper.class)
 @RegisterRestClient(configKey = "github-service")
 public interface GitHubServiceClient
         extends UserRoutes, RepositoryRoutes, ContributionRoutes {}
