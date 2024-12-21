e2a9531be462f90fc9b7503418817edcb87f239a
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2024-12-21 04:19:33 +0200

    feat: Add method to find repository by owner and name

--- src/main/java/com/stergion/githubbackend/infrastructure/persistence/repositories/RepositoryRepository.java
+++ src/main/java/com/stergion/githubbackend/infrastructure/persistence/repositories/RepositoryRepository.java
@@ -3,4 +3,7 @@ package com.stergion.githubbackend.infrastructure.persistence.repositories;
 import io.quarkus.mongodb.panache.PanacheMongoRepository;
 
 public class RepositoryRepository implements PanacheMongoRepository<Repository> {
+    public Repository findByNameAndOwner(String owner, String name) {
+        return find("owner = ?1 and name = ?2", owner, name).firstResult();
+    }
 }
