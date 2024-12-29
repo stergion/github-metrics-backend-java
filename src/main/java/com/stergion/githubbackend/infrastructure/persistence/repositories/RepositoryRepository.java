bdc3af7bfa3a7c56b808f98fe33138376f30e038
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2024-12-30 00:14:30 +0200

    fix: Add @ApplicationScoped annotation to RepositoryRepository

--- src/main/java/com/stergion/githubbackend/infrastructure/persistence/repositories/RepositoryRepository.java
+++ src/main/java/com/stergion/githubbackend/infrastructure/persistence/repositories/RepositoryRepository.java
@@ -1,7 +1,9 @@
 package com.stergion.githubbackend.infrastructure.persistence.repositories;
 
 import io.quarkus.mongodb.panache.PanacheMongoRepository;
+import jakarta.enterprise.context.ApplicationScoped;
 
+@ApplicationScoped
 public class RepositoryRepository implements PanacheMongoRepository<Repository> {
     public Repository findByNameAndOwner(String owner, String name) {
         return find("owner = ?1 and name = ?2", owner, name).firstResult();
