a463a5daa037dbbb8f1a758793129541c8e449de
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2025-01-10 15:04:50 +0200

    feat: Implement getRepositoriesCommittedToBatched() method to fetch repos in batches

--- src/main/java/com/stergion/githubbackend/infrastructure/external/githubservice/service/RepositoryClient.java
+++ src/main/java/com/stergion/githubbackend/infrastructure/external/githubservice/service/RepositoryClient.java
@@ -1,5 +1,7 @@
 package com.stergion.githubbackend.infrastructure.external.githubservice.service;
 
+import com.stergion.githubbackend.common.batch.BatchProcessor;
+import com.stergion.githubbackend.common.batch.BatchProcessorConfig;
 import com.stergion.githubbackend.domain.repositories.RepositoryDTO;
 import com.stergion.githubbackend.infrastructure.external.githubservice.client.GitHubServiceClient;
 import com.stergion.githubbackend.infrastructure.external.githubservice.client.mappers.RepositoryGHMapper;
@@ -11,6 +13,7 @@ import jakarta.inject.Inject;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Objects;
 
 @ApplicationScoped
@@ -35,6 +38,21 @@ public class RepositoryClient {
                      .map(event -> transformer.transform(event, RepositoryGH.class))
                      .filter(Objects::nonNull)
                      .map(mapper::toDTO);
+
+    }
+
+    public Multi<List<RepositoryDTO>> getRepositoriesCommittedToBatched(String login,
+                                                                        LocalDate from,
+                                                                        LocalDate to,
+                                                                        BatchProcessorConfig config) {
+        BatchProcessor batchProcessor = new BatchProcessor(config);
+
+        var stream = client.getRepositoriesCommittedTo(login, from, to)
+                           .map(event -> transformer.transform(event, RepositoryGH.class));
+
+        return batchProcessor.processBatch(stream)
+                             .transform(mapper::toDTO)
+                             .toMulti();
 
     }
 
