0df9da0da3632398744006078ee68556a232665e
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2025-01-11 21:47:00 +0200

    feat: Create ValidationException.java

---
+++ src/main/java/com/stergion/githubbackend/domain/contirbutions/fetch/ValidationException.java
@@ -0,0 +1,7 @@
+package com.stergion.githubbackend.domain.contirbutions.fetch;
+
+public class ValidationException extends RuntimeException {
+    public ValidationException(String message) {
+        super(message);
+    }
+}
