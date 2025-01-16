6b206cc174e605e576ec93e1daf3b0f4b1d6ff0a
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2025-01-16 18:30:28 +0200

    feat: Overrode persist() and update() methods in UserRepository to automatically set createdAt and updatedAt on all persistence operations

--- src/main/java/com/stergion/githubbackend/infrastructure/persistence/users/UserRepository.java
+++ src/main/java/com/stergion/githubbackend/infrastructure/persistence/users/UserRepository.java
@@ -3,8 +3,37 @@ package com.stergion.githubbackend.infrastructure.persistence.users;
 import io.quarkus.mongodb.panache.PanacheMongoRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 
+import java.time.LocalDate;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
 @ApplicationScoped
 public class UserRepository implements PanacheMongoRepository<User> {
+    @Override
+    public void persist(User user) {
+        setTimestamps(user);
+        PanacheMongoRepository.super.persist(user);
+    }
+
+    @Override
+    public void update(User user) {
+        setTimestamps(user);
+        PanacheMongoRepository.super.update(user);
+    }
+    @Override
+    public void update(Iterable<User> users) {
+        users.forEach(this::setTimestamps);
+
+        PanacheMongoRepository.super.update(users);
+    }
+
+    @Override
+    public void update(Stream<User> users) {
+        Stream<User> users2 = users.peek((Consumer<? super User>) this::setTimestamps);
+
+        PanacheMongoRepository.super.update(users2);
+    }
+
     public void delete(String login) {
         delete("login", login);
     }
@@ -13,4 +42,10 @@ public class UserRepository implements PanacheMongoRepository<User> {
         return find("login", login).firstResult();
     }
 
+    private void setTimestamps(User user) {
+        if (user.id == null) {
+            user.createdAt = LocalDate.now();
+        }
+        user.updatedAt = LocalDate.now();
+    }
 }
