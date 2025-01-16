bf1a0d1def4d81741250e4b52e00076ad2ac9d7a
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2025-01-16 18:18:33 +0200

    feat: Added createdAt, updatedAt parameters in UserDTO to keep track of user creation and last update date

--- src/main/java/com/stergion/githubbackend/domain/users/UserDTO.java
+++ src/main/java/com/stergion/githubbackend/domain/users/UserDTO.java
@@ -7,9 +7,11 @@ import com.stergion.githubbackend.domain.utils.types.Github;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 import org.bson.types.ObjectId;
 
 import java.net.URI;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Objects;
 
@@ -27,6 +29,12 @@ public record UserDTO(
         @Email
         String email,
 
+        @PastOrPresent
+        LocalDate createdAt,
+
+        @PastOrPresent
+        LocalDate updatedAt,
+
         @NotNull
         List<ObjectId> repositories,
 
@@ -38,6 +46,7 @@ public record UserDTO(
     public UserDTO {
         repositories = repositories != null ? List.copyOf(repositories) : List.of();
     }
+
     static ObjectMapper mapper = JsonObjectMapper.create();
 
     @Override
