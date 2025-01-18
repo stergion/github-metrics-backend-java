f779a85c28792a339a7368df9d5c7bd6465d683d
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2025-01-18 02:09:34 +0200

    refactor: Updated class IssueComment to conform with interface Contribution

--- src/main/java/com/stergion/githubbackend/infrastructure/persistence/contirbutions/entities/IssueComment.java
+++ src/main/java/com/stergion/githubbackend/infrastructure/persistence/contirbutions/entities/IssueComment.java
@@ -1,23 +1,97 @@
 package com.stergion.githubbackend.infrastructure.persistence.contirbutions.entities;
 
 import com.stergion.githubbackend.infrastructure.persistence.utilityTypes.Github;
+import com.stergion.githubbackend.infrastructure.persistence.utilityTypes.NameWithOwner;
+import com.stergion.githubbackend.infrastructure.persistence.utilityTypes.UserWithLogin;
 import io.quarkus.mongodb.panache.common.MongoEntity;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
+import org.bson.codecs.pojo.annotations.BsonProperty;
+import org.bson.types.ObjectId;
 
 import java.time.LocalDate;
 
 @MongoEntity(collection = "issueComments")
-public non-sealed class IssueComment extends Contribution {
+public non-sealed class IssueComment implements Contribution {
+    ObjectId id;
+
+    @NotNull
+    @BsonProperty("user_id")
+    ObjectId userId;
+
+    @NotNull
+    @BsonProperty("repository_id")
+    ObjectId repositoryId;
+
+    @NotNull
+    UserWithLogin user;
+    @NotNull
+    NameWithOwner repository;
+
+    @NotNull
+    Github github;
     @NotNull
     @PastOrPresent
-    public LocalDate createdAt;
+    LocalDate createdAt;
+    LocalDate publishedAt;
+    LocalDate updatedAt;
+    LocalDate lastEditedAt;
+    AssociatedIssue associatedIssue;
+    String body;
 
-    public LocalDate publishedAt;
-    public LocalDate updatedAt;
-    public LocalDate lastEditedAt;
-    public AssociatedIssue associatedIssue;
-    public String body;
+    @Override
+    public ObjectId id() {
+        return id;
+    }
+
+    @Override
+    public ObjectId userId() {
+        return userId;
+    }
+
+    @Override
+    public ObjectId repositoryId() {
+        return repositoryId;
+    }
+
+    @Override
+    public UserWithLogin user() {
+        return user;
+    }
+
+    @Override
+    public NameWithOwner repository() {
+        return repository;
+    }
+
+    @Override
+    public Github github() {
+        return github;
+    }
+
+    public LocalDate createdAt() {
+        return createdAt;
+    }
+
+    public LocalDate publishedAt() {
+        return publishedAt;
+    }
+
+    public LocalDate updatedAt() {
+        return updatedAt;
+    }
+
+    public LocalDate lastEditedAt() {
+        return lastEditedAt;
+    }
+
+    public AssociatedIssue associatedIssue() {
+        return associatedIssue;
+    }
+
+    public String body() {
+        return body;
+    }
 
     public static class AssociatedIssue {
         public IssueType type;
@@ -31,6 +105,7 @@ public non-sealed class IssueComment extends Contribution {
                     "}";
         }
     }
+
     public enum IssueType {
         ISSUE,
         PULL_REQUEST,
