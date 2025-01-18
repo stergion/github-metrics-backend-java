9672a76f93ae6ffd68566d7d5c7b5f177d871c28
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2025-01-18 02:17:26 +0200

    feat: Added constructor for UserWithLogin class

--- src/main/java/com/stergion/githubbackend/infrastructure/persistence/utilityTypes/UserWithLogin.java
+++ src/main/java/com/stergion/githubbackend/infrastructure/persistence/utilityTypes/UserWithLogin.java
@@ -3,6 +3,9 @@ package com.stergion.githubbackend.infrastructure.persistence.utilityTypes;
 public class UserWithLogin {
     public String login;
 
+    public UserWithLogin(String login) {
+        this.login = login;
+    }
     @Override
     public String toString() {
         return "{\"user\": %s}".formatted(login);
