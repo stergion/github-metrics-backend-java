362a584049f880d6bfde02522c558ab4b8a96a80
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2025-01-18 02:33:13 +0200

    refactor: added public no-args constructor

--- src/main/java/com/stergion/githubbackend/infrastructure/persistence/utilityTypes/UserWithLogin.java
+++ src/main/java/com/stergion/githubbackend/infrastructure/persistence/utilityTypes/UserWithLogin.java
@@ -3,9 +3,13 @@ package com.stergion.githubbackend.infrastructure.persistence.utilityTypes;
 public class UserWithLogin {
     public String login;
 
+    public UserWithLogin() {
+    }
+
     public UserWithLogin(String login) {
         this.login = login;
     }
+
     @Override
     public String toString() {
         return "{\"user\": %s}".formatted(login);
