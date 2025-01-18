4f10f1ccab0564d5e0defe042220df80f0d719f6
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2025-01-18 03:30:05 +0200

    feat: added ability to find issue contributions by user id and state

--- src/main/java/com/stergion/githubbackend/infrastructure/persistence/contirbutions/repositories/IssueRepository.java
+++ src/main/java/com/stergion/githubbackend/infrastructure/persistence/contirbutions/repositories/IssueRepository.java
@@ -1,6 +1,7 @@
 package com.stergion.githubbackend.infrastructure.persistence.contirbutions.repositories;
 
 import com.stergion.githubbackend.infrastructure.persistence.contirbutions.entities.Issue;
+import com.stergion.githubbackend.infrastructure.persistence.utilityTypes.IssueState;
 import io.quarkus.mongodb.panache.PanacheMongoRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.bson.types.ObjectId;
@@ -10,4 +11,7 @@ import java.util.List;
 
 @ApplicationScoped
 public final class IssueRepository implements ContributionRepository<Issue> {
+    public List<Issue> findByUserIdAndState(ObjectId testUserId, IssueState issueState) {
+        return list("userId =?1 and state =?2", testUserId, issueState);
+    }
 }
