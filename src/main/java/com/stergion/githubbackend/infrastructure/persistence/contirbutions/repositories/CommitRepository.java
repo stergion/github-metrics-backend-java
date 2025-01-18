2a65cc793fab1a9722de978d82ff94549671afad
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2025-01-18 02:13:17 +0200

    chore: Cleanup imports

--- src/main/java/com/stergion/githubbackend/infrastructure/persistence/contirbutions/repositories/CommitRepository.java
+++ src/main/java/com/stergion/githubbackend/infrastructure/persistence/contirbutions/repositories/CommitRepository.java
@@ -1,12 +1,7 @@
 package com.stergion.githubbackend.infrastructure.persistence.contirbutions.repositories;
 
 import com.stergion.githubbackend.infrastructure.persistence.contirbutions.entities.Commit;
-import io.quarkus.mongodb.panache.PanacheMongoRepository;
 import jakarta.enterprise.context.ApplicationScoped;
-import org.bson.types.ObjectId;
-import org.jboss.resteasy.reactive.common.NotImplementedYet;
-
-import java.util.List;
 
 @ApplicationScoped
 public final class CommitRepository implements ContributionRepository<Commit> {
