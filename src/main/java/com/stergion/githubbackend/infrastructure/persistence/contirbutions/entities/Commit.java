ffdebcd5b2e50feb6bce12814e51dad831dc2811
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2025-01-18 02:09:07 +0200

    refactor: Updated class Commit to conform with interface Contribution

--- src/main/java/com/stergion/githubbackend/infrastructure/persistence/contirbutions/entities/Commit.java
+++ src/main/java/com/stergion/githubbackend/infrastructure/persistence/contirbutions/entities/Commit.java
@@ -1,17 +1,33 @@
 package com.stergion.githubbackend.infrastructure.persistence.contirbutions.entities;
 
-import com.stergion.githubbackend.infrastructure.persistence.utilityTypes.CommitComment;
-import com.stergion.githubbackend.infrastructure.persistence.utilityTypes.File;
-import com.stergion.githubbackend.infrastructure.persistence.utilityTypes.Github;
+import com.stergion.githubbackend.infrastructure.persistence.utilityTypes.*;
 import io.quarkus.mongodb.panache.common.MongoEntity;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
+import org.bson.codecs.pojo.annotations.BsonProperty;
+import org.bson.types.ObjectId;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @MongoEntity(collection = "commits")
-public non-sealed class Commit extends Contribution {
+public non-sealed class Commit implements Contribution {
+    public ObjectId id;
+
+    @NotNull
+    @BsonProperty("user_id")
+    public ObjectId userId;
+    @NotNull
+    @BsonProperty("repository_id")
+    public ObjectId repositoryId;
+
+    @NotNull
+    public UserWithLogin user;
+    @NotNull
+    public NameWithOwner repository;
+
+    @NotNull
+    public Github github;
 
     @NotNull
     @PastOrPresent
@@ -43,5 +59,75 @@ public non-sealed class Commit extends Contribution {
                 ", files: " + files +
                 ", filesCount: " + filesCount +
                 "}";
+    }
+
+    @Override
+    public ObjectId id() {
+        return id;
+    }
+
+    @Override
+    public ObjectId userId() {
+        return userId;
+    }
+
+    @Override
+    public ObjectId repositoryId() {
+        return repositoryId;
+    }
+
+    @Override
+    public UserWithLogin user() {
+        return user;
+    }
+
+    @Override
+    public NameWithOwner repository() {
+        return repository;
+    }
+
+    @Override
+    public Github github() {
+        return github;
+    }
+
+    public LocalDate committedDate() {
+        return committedDate;
+    }
+
+    public LocalDate pushedDate() {
+        return pushedDate;
+    }
+
+    public int additions() {
+        return additions;
+    }
+
+    public int deletions() {
+        return deletions;
+    }
+
+    public List<CommitComment> comments() {
+        return comments;
+    }
+
+    public int commentsCount() {
+        return commentsCount;
+    }
+
+    public List<Github> associatedPullRequest() {
+        return associatedPullRequest;
+    }
+
+    public int associatedPullRequestsCount() {
+        return associatedPullRequestsCount;
+    }
+
+    public List<File> files() {
+        return files;
+    }
+
+    public int filesCount() {
+        return filesCount;
     }
 }
