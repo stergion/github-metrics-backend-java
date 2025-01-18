f62326eb71b332125333b6d040aee4a8064c5f10
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2025-01-18 02:09:22 +0200

    refactor: Updated class Issue to conform with interface Contribution

--- src/main/java/com/stergion/githubbackend/infrastructure/persistence/contirbutions/entities/Issue.java
+++ src/main/java/com/stergion/githubbackend/infrastructure/persistence/contirbutions/entities/Issue.java
@@ -1,28 +1,45 @@
 package com.stergion.githubbackend.infrastructure.persistence.contirbutions.entities;
 
-import com.stergion.githubbackend.infrastructure.persistence.utilityTypes.IssueState;
-import com.stergion.githubbackend.infrastructure.persistence.utilityTypes.Label;
+import com.stergion.githubbackend.infrastructure.persistence.utilityTypes.*;
 import io.quarkus.mongodb.panache.common.MongoEntity;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
+import org.bson.codecs.pojo.annotations.BsonProperty;
+import org.bson.types.ObjectId;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @MongoEntity(collection = "issues")
-public non-sealed class Issue extends Contribution {
+public non-sealed class Issue implements Contribution {
+    ObjectId id;
+
+    @NotNull
+    @BsonProperty("user_id")
+    ObjectId userId;
+
+    @NotNull
+    @BsonProperty("repository_id")
+    ObjectId repositoryId;
+
+    @NotNull
+    UserWithLogin user;
+    @NotNull
+    NameWithOwner repository;
+
+    @NotNull
+    Github github;
     @NotNull
     @PastOrPresent
-    public LocalDate createdAt;
-
-    public LocalDate closedAt;
-    public LocalDate updatedAt;
-    public IssueState state;
-    public String title;
-    public String body;
-    public int reactionsCount;
-    public List<Label> labels;
-    public String closer;
+    LocalDate createdAt;
+    LocalDate closedAt;
+    LocalDate updatedAt;
+    IssueState state;
+    String title;
+    String body;
+    int reactionsCount;
+    List<Label> labels;
+    String closer;
 
     @Override
     public String toString() {
@@ -40,5 +57,71 @@ public non-sealed class Issue extends Contribution {
                 ", labels: " + labels +
                 ", closer: '" + closer + '\'' +
                 "}";
+    }
+
+    @Override
+    public ObjectId id() {
+        return id;
+    }
+
+    @Override
+    public ObjectId userId() {
+        return userId;
+    }
+
+    @Override
+    public ObjectId repositoryId() {
+        return repositoryId;
+    }
+
+    @Override
+    public UserWithLogin user() {
+        return user;
+    }
+
+    @Override
+    public NameWithOwner repository() {
+        return repository;
+    }
+
+    @Override
+    public Github github() {
+        return github;
+    }
+
+    public LocalDate createdAt() {
+        return createdAt;
+    }
+
+    public LocalDate closedAt() {
+        return closedAt;
+    }
+
+    public LocalDate updatedAt() {
+        return updatedAt;
+    }
+
+    public IssueState state() {
+        return state;
+    }
+
+    public String title() {
+        return title;
+    }
+
+    public String body() {
+        return body;
+    }
+
+    public int reactionsCount() {
+        return reactionsCount;
+    }
+
+    public List<Label> labels() {
+        return labels;
+    }
+
+    public String closer() {
+        return closer;
     }
 }
