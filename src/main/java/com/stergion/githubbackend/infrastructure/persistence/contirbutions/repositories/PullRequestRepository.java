680806f1a54a512c670ad5e4dd658930c38f8fb0
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2025-01-18 03:29:37 +0200

    feat: added ability to find pull request contributions by user id and state

--- src/main/java/com/stergion/githubbackend/infrastructure/persistence/contirbutions/repositories/PullRequestRepository.java
+++ src/main/java/com/stergion/githubbackend/infrastructure/persistence/contirbutions/repositories/PullRequestRepository.java
@@ -1,6 +1,9 @@
 package com.stergion.githubbackend.infrastructure.persistence.contirbutions.repositories;
 
+import com.stergion.githubbackend.infrastructure.persistence.contirbutions.entities.Issue;
 import com.stergion.githubbackend.infrastructure.persistence.contirbutions.entities.PullRequest;
+import com.stergion.githubbackend.infrastructure.persistence.utilityTypes.IssueState;
+import com.stergion.githubbackend.infrastructure.persistence.utilityTypes.PullRequestState;
 import io.quarkus.mongodb.panache.PanacheMongoRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.bson.types.ObjectId;
@@ -10,4 +13,7 @@ import java.util.List;
 
 @ApplicationScoped
 public final class PullRequestRepository implements ContributionRepository<PullRequest> {
+    public List<PullRequest> findByUserIdAndState(ObjectId testUserId, PullRequestState prState) {
+        return list("userId =?1 and state =?2", testUserId, prState);
+    }
 }
