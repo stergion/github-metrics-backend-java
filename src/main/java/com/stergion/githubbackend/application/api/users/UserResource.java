e8b0630f725891f4f7f13f18efb35635f78c51de
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2025-01-22 03:15:09 +0200

    feat: Implement user resource with basic CRUD operations

--- src/main/java/com/stergion/githubbackend/application/api/users/UserResource.java
+++ src/main/java/com/stergion/githubbackend/application/api/users/UserResource.java
@@ -1,62 +1,108 @@
 package com.stergion.githubbackend.application.api.users;
 
-import jakarta.json.Json;
-import jakarta.json.JsonObject;
+import com.stergion.githubbackend.application.mapper.RepositoryMapper;
+import com.stergion.githubbackend.application.mapper.UserMapper;
+import com.stergion.githubbackend.application.response.NameWithOwnerResponse;
+import com.stergion.githubbackend.application.response.UserResponse;
+import com.stergion.githubbackend.application.service.UserDataManagementService;
+import com.stergion.githubbackend.domain.repositories.RepositoryDTO;
+import com.stergion.githubbackend.domain.repositories.RepositoryService;
+import com.stergion.githubbackend.domain.users.UserService;
+import io.quarkus.logging.Log;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import org.jboss.resteasy.reactive.RestResponse;
 import org.jboss.resteasy.reactive.RestResponse.ResponseBuilder;
 
+import java.util.List;
+
 @Path("/api/users/{login}")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
+    @Inject
+    UserService userService;
+    @Inject
+    RepositoryService repositoryService;
+    @Inject
+    UserMapper userMapper;
+    @Inject
+    RepositoryMapper repositoryMapper;
+    @Inject
+    UserDataManagementService userDataManagementService;
 
     @GET
-    public RestResponse<JsonObject> getUser(String login) {
-        // TODO: Implement route
+    public Object getUser(String login) {
+        Log.info("Fetching user: " + login);
+        try {
+            var usr = userService.getUser(login);
+            List<RepositoryDTO> repos = repositoryService.getRepositories(usr.repositories());
+            UserResponse userResponse = userMapper.toResponse(usr, repos);
+            return ResponseBuilder.ok(userResponse).build();
+        } catch (Exception e) {
+            Log.error(e);
+            return ResponseBuilder.serverError().build();
+        }
 
-        var obj = Json.createObjectBuilder()
-                      .add("login", login)
-                      .add("message", "GET  /users/" + login)
-                      .build();
-        System.out.println(obj);
-        return ResponseBuilder.ok(obj).build();
     }
 
     @POST
-    public RestResponse<JsonObject> createUser(String login) {
-        // TODO: Implement route
+    public RestResponse<Object> createUser(String login) {
+        Log.info("Creating user: " + login);
+        try {
+            userDataManagementService.setupUser(login);
+        } catch (Exception e) {
+            Log.error(e);
+            return ResponseBuilder.serverError().build();
+        }
 
-        var obj = Json.createObjectBuilder()
-                      .add("login", login)
-                      .add("message", "User created!")
-                      .build();
-        System.out.println(obj);
-        return ResponseBuilder.ok(obj).build();
+        Log.info("User '" + login + "' was created successfully!!!");
+        return ResponseBuilder.create(201).build();
     }
 
     @PUT
-    public RestResponse<JsonObject> updateUser(String login) {
-        // TODO: Implement route
+    public RestResponse<Object> updateUser(String login) {
+        Log.info("Updating user: " + login);
+        try {
+            userDataManagementService.updateUser(login);
+        } catch (Exception e) {
+            Log.error(e);
+            return ResponseBuilder.serverError().build();
+        }
 
-        var obj = Json.createObjectBuilder()
-                      .add("login", login)
-                      .add("message", "user updated!")
-                      .build();
-        System.out.println(obj);
-        return ResponseBuilder.ok(obj).build();
+        Log.info("User '" + login + "' was updated successfully!!!");
+        return ResponseBuilder.ok().build();
     }
 
     @DELETE
-    public RestResponse<JsonObject> deleteUser(String login) {
-        // TODO: Implement route
+    public RestResponse<Object> deleteUser(String login) {
+        Log.info("Deleting user: " + login);
+        try {
+            userDataManagementService.deleteUser(login);
+        } catch (Exception e) {
+            Log.error(e);
+            return ResponseBuilder.serverError().build();
+        }
 
-        var obj = Json.createObjectBuilder()
-                      .add("login", login)
-                      .add("message", "user deleted!")
-                      .build();
-        System.out.println(obj);
-        return ResponseBuilder.ok(obj).build();
+        Log.info("User '" + login + "' was deleted successfully!!!");
+        return ResponseBuilder.create(200).build();
+    }
+
+    @GET
+    @Path("/repositories")
+    public Object getUserRepositories(String login) {
+        Log.info("Getting user: " + login + " repositories");
+        try {
+            List<RepositoryDTO> repos = userService.getUserRepositories(login);
+            List<NameWithOwnerResponse> nameWithOwners = repos.stream()
+                                                              .map(repositoryMapper::toNameWithOwnerResponse)
+                                                              .toList();
+            return ResponseBuilder.ok(nameWithOwners).build();
+        } catch (Exception e) {
+            Log.error(e);
+            return ResponseBuilder.serverError().build();
+        }
+
     }
 }
