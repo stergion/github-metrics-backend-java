1bc8364ce28914c511e74ba1c16f3273f9a0a4dc
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2025-01-25 01:13:15 +0200

    feat: Implement MongoIssueCommentSearchStrategy class for searching issue comments in Mongo databases

---
+++ src/main/java/com/stergion/githubbackend/infrastructure/persistence/contirbutions/search/MongoIssueCommentSearchStrategy.java
@@ -0,0 +1,43 @@
+package com.stergion.githubbackend.infrastructure.persistence.contirbutions.search;
+
+import com.mongodb.client.model.Filters;
+import com.stergion.githubbackend.domain.contirbutions.search.IssueCommentSearchStrategy;
+import com.stergion.githubbackend.domain.contirbutions.search.criteria.IssueCommentSearchCriteria;
+import com.stergion.githubbackend.domain.contirbutions.search.fields.CommonField;
+import com.stergion.githubbackend.domain.contirbutions.search.fields.IssueCommentField;
+import com.stergion.githubbackend.infrastructure.persistence.contirbutions.entities.IssueComment;
+import com.stergion.githubbackend.infrastructure.persistence.contirbutions.repositories.IssueCommentRepository;
+import org.bson.conversions.Bson;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MongoIssueCommentSearchStrategy
+        extends MongoContributionSearchStrategy<IssueComment, IssueCommentSearchCriteria>
+        implements IssueCommentSearchStrategy {
+
+    public MongoIssueCommentSearchStrategy(IssueCommentRepository repository) {
+        super(repository);
+    }
+
+    @Override
+    public Bson createQuery(IssueCommentSearchCriteria criteria) {
+        List<Bson> conditions = new ArrayList<>();
+        conditions.add(
+                Filters.eq(CommonField.USER_LOGIN.fieldName(), criteria.getUserLogin()));
+
+        criteria.getOwner().ifPresent(owner ->
+                conditions.add(Filters.eq(CommonField.OWNER.fieldName(), owner)));
+
+        criteria.getName().ifPresent(name ->
+                conditions.add(Filters.eq(CommonField.NAME.fieldName(), name)));
+
+        criteria.getSince().ifPresent(since ->
+                conditions.add(Filters.gte(IssueCommentField.PUBLISHED_AT.fieldName(), since)));
+
+        criteria.getUntil().ifPresent(until ->
+                conditions.add(Filters.lte(IssueCommentField.PUBLISHED_AT.fieldName(), until)));
+
+        return Filters.and(conditions);
+    }
+}
