b20df400656c4584476049152d508029a7a76f54
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2025-01-25 01:15:14 +0200

    feat: Add Database qualifier annotation for database implementation selection

---
+++ src/main/java/com/stergion/githubbackend/common/Database.java
@@ -0,0 +1,30 @@
+package com.stergion.githubbackend.common;
+
+import jakarta.enterprise.util.AnnotationLiteral;
+import jakarta.inject.Qualifier;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.TYPE})
+public @interface Database {
+    DatabaseType value();  // "mongo" or "postgres"
+
+    @SuppressWarnings("ClassExplicitlyAnnotation")
+    final class Literal extends AnnotationLiteral<Database> implements Database {
+        private final DatabaseType value;
+
+        public Literal(DatabaseType value) {
+            this.value = value;
+        }
+
+        @Override
+        public DatabaseType value() {
+            return value;
+        }
+    }
+}
