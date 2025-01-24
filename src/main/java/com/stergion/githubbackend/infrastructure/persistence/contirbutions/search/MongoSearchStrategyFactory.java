cc044e411ef5154d679549d582abd5034a6aa9de
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2025-01-25 01:14:27 +0200

    feat: Add MongoSearchStrategyFactory

---
+++ src/main/java/com/stergion/githubbackend/infrastructure/persistence/contirbutions/search/MongoSearchStrategyFactory.java
@@ -0,0 +1,49 @@
+package com.stergion.githubbackend.infrastructure.persistence.contirbutions.search;
+
+import com.stergion.githubbackend.common.Database;
+import com.stergion.githubbackend.common.DatabaseType;
+import com.stergion.githubbackend.domain.contirbutions.search.*;
+import com.stergion.githubbackend.infrastructure.persistence.contirbutions.repositories.*;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+@ApplicationScoped
+@Database(DatabaseType.MONGO)
+public class MongoSearchStrategyFactory implements SearchStrategyFactory {
+    @Inject
+    CommitRepository commitRepository;
+    @Inject
+    IssueRepository issueRepository;
+    @Inject
+    PullRequestRepository pullRequestRepository;
+    @Inject
+    PullRequestReviewRepository pullRequestReviewRepository;
+    @Inject
+    IssueCommentRepository issueCommentRepository;
+
+    @Override
+    public CommitSearchStrategy createCommitSearchStrategy() {
+        return new MongoCommitSearchStrategy(commitRepository);
+    }
+
+    @Override
+    public IssueSearchStrategy createIssueSearchStrategy() {
+        return new MongoIssueSearchStrategy(issueRepository);
+    }
+
+    @Override
+    public PullRequestSearchStrategy createPullRequestSearchStrategy() {
+        return new MongoPullRequestSearchStrategy(pullRequestRepository);
+    }
+
+    @Override
+    public PullRequestReviewSearchStrategy createPullRequestReviewSearchStrategy() {
+        return new MongoPullRequestReviewSearchStrategy(pullRequestReviewRepository);
+    }
+    
+    @Override
+    public IssueCommentSearchStrategy createIssueCommentSearchStrategy() {
+        return new MongoIssueCommentSearchStrategy(issueCommentRepository);
+    }
+
+}
