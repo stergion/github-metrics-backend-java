a0ac863396765b29bd43b1adc6af19dfc8fd24c9
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2025-01-25 01:16:02 +0200

    feat: Add DatabaseType enum for supported database implementations

---
+++ src/main/java/com/stergion/githubbackend/common/DatabaseType.java
@@ -0,0 +1,25 @@
+package com.stergion.githubbackend.common;
+
+public enum DatabaseType {
+    MONGO("mongo"),
+    POSTGRES("postgres");
+
+    private final String value;
+
+    DatabaseType(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public static DatabaseType fromString(String value) {
+        for (DatabaseType type : values()) {
+            if (type.value.equalsIgnoreCase(value)) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("Unknown database type: " + value);
+    }
+}
