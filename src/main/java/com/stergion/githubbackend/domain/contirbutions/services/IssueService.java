44a0ab287fdcfef5afa60a7fa4261b7f5e3ae933
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2025-01-25 01:21:08 +0200

    feat: add issue search functionality

--- src/main/java/com/stergion/githubbackend/domain/contirbutions/services/IssueService.java
+++ src/main/java/com/stergion/githubbackend/domain/contirbutions/services/IssueService.java
@@ -4,9 +4,13 @@ import com.stergion.githubbackend.domain.contirbutions.dto.IssueDTO;
 import com.stergion.githubbackend.domain.contirbutions.fetch.FetchParams;
 import com.stergion.githubbackend.domain.contirbutions.fetch.IssueFetchStrategy;
 import com.stergion.githubbackend.domain.contirbutions.mappers.IssueMapper;
+import com.stergion.githubbackend.domain.contirbutions.search.IssueSearchStrategy;
+import com.stergion.githubbackend.domain.contirbutions.search.PagedResponse;
+import com.stergion.githubbackend.domain.contirbutions.search.criteria.IssueSearchCriteria;
 import com.stergion.githubbackend.infrastructure.persistence.contirbutions.entities.Issue;
 import com.stergion.githubbackend.infrastructure.persistence.contirbutions.repositories.IssueRepository;
 import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.bson.types.ObjectId;
@@ -18,6 +22,8 @@ import java.util.List;
 public class IssueService extends ContributionService<IssueDTO, Issue> {
     @Inject
     IssueMapper issueMapper;
+    @Inject
+    IssueSearchStrategy searchStrategy;
 
     @Inject
     public IssueService(IssueRepository issueRepository, IssueFetchStrategy fetchStrategy) {
@@ -34,12 +40,18 @@ public class IssueService extends ContributionService<IssueDTO, Issue> {
         return issueMapper.toDTO(entity);
     }
 
-    public Multi<List<IssueDTO>> fetchAndCreateIssues(String login, LocalDateTime from, LocalDateTime to) {
+    public Multi<List<IssueDTO>> fetchAndCreateIssues(String login, LocalDateTime from,
+                                                      LocalDateTime to) {
         var params = FetchParams.builder()
                                 .login(login)
                                 .dateRange(from, to)
                                 .build();
 
         return fetchAndCreate(params);
+    }
+
+    public Uni<PagedResponse<IssueDTO>> search(IssueSearchCriteria criteria) {
+        return searchStrategy.search(criteria)
+                             .map(response -> PagedResponse.map(response, issueMapper::toDTO));
     }
 }
