25f70160b4de76ef11f303355ead04b6ea83c1ea
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2025-01-25 01:20:59 +0200

    feat: add issue comment search functionality

--- src/main/java/com/stergion/githubbackend/domain/contirbutions/services/IssueCommentService.java
+++ src/main/java/com/stergion/githubbackend/domain/contirbutions/services/IssueCommentService.java
@@ -4,9 +4,13 @@ import com.stergion.githubbackend.domain.contirbutions.dto.IssueCommentDTO;
 import com.stergion.githubbackend.domain.contirbutions.fetch.FetchParams;
 import com.stergion.githubbackend.domain.contirbutions.fetch.IssueCommentFetchStrategy;
 import com.stergion.githubbackend.domain.contirbutions.mappers.IssueCommentMapper;
+import com.stergion.githubbackend.domain.contirbutions.search.IssueCommentSearchStrategy;
+import com.stergion.githubbackend.domain.contirbutions.search.PagedResponse;
+import com.stergion.githubbackend.domain.contirbutions.search.criteria.IssueCommentSearchCriteria;
 import com.stergion.githubbackend.infrastructure.persistence.contirbutions.entities.IssueComment;
 import com.stergion.githubbackend.infrastructure.persistence.contirbutions.repositories.IssueCommentRepository;
 import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.bson.types.ObjectId;
@@ -18,6 +22,8 @@ import java.util.List;
 public class IssueCommentService extends ContributionService<IssueCommentDTO, IssueComment> {
     @Inject
     IssueCommentMapper issueCommentMapper;
+    @Inject
+    IssueCommentSearchStrategy searchStrategy;
 
     @Inject
     public IssueCommentService(IssueCommentRepository issueCommentRepository,
@@ -36,7 +42,8 @@ public class IssueCommentService extends ContributionService<IssueCommentDTO, Is
         return issueCommentMapper.toDTO(entity);
     }
 
-    public Multi<List<IssueCommentDTO>> fetchAndCreateIssueComments(String login, LocalDateTime from,
+    public Multi<List<IssueCommentDTO>> fetchAndCreateIssueComments(String login,
+                                                                    LocalDateTime from,
                                                                     LocalDateTime to) {
         var params = FetchParams.builder()
                                 .login(login)
@@ -44,5 +51,11 @@ public class IssueCommentService extends ContributionService<IssueCommentDTO, Is
                                 .build();
 
         return fetchAndCreate(params);
+    }
+
+    public Uni<PagedResponse<IssueCommentDTO>> search(IssueCommentSearchCriteria criteria) {
+        return searchStrategy.search(criteria)
+                             .map(response -> PagedResponse.map(response,
+                                     issueCommentMapper::toDTO));
     }
 }
