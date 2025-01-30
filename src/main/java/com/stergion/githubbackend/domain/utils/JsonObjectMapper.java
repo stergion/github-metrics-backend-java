52bfa2865ac110963b06f93b8d8f48fd86605180
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2025-01-30 02:53:32 +0200

    feat: register Jdk8Module to JsonObjectMapper

--- src/main/java/com/stergion/githubbackend/domain/utils/JsonObjectMapper.java
+++ src/main/java/com/stergion/githubbackend/domain/utils/JsonObjectMapper.java
@@ -2,6 +2,7 @@ package com.stergion.githubbackend.domain.utils;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.util.StdDateFormat;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public final class JsonObjectMapper {
@@ -12,6 +13,7 @@ public final class JsonObjectMapper {
     public static ObjectMapper create() {
         return new ObjectMapper()
                 .registerModule(new JavaTimeModule())
+                .registerModule(new Jdk8Module())
                 .setDateFormat(new StdDateFormat().withColonInTimeZone(true));
     }
 }
