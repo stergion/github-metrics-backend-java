3c05381891be2cab7ca5c5223d41918f3076d379
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2025-02-06 01:05:38 +0200

    refactor: add getters/setters in postgres File entity

--- src/main/java/com/stergion/githubbackend/infrastructure/persistence/postgres/utils/types/File.java
+++ src/main/java/com/stergion/githubbackend/infrastructure/persistence/postgres/utils/types/File.java
@@ -36,4 +36,89 @@ public class File {
                     deletions);
         }
     }
+
+    /*
+     *********************
+     * Getters / Setters *
+     *********************
+     */
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getBaseName() {
+        return baseName;
+    }
+
+    public void setBaseName(String baseName) {
+        this.baseName = baseName;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getAdditions() {
+        return additions;
+    }
+
+    public void setAdditions(int additions) {
+        this.additions = additions;
+    }
+
+    public int getDeletions() {
+        return deletions;
+    }
+
+    public void setDeletions(int deletions) {
+        this.deletions = deletions;
+    }
+
+    public int getChanges() {
+        return changes;
+    }
+
+    public void setChanges(int changes) {
+        this.changes = changes;
+    }
+
+    public String getPatch() {
+        return patch;
+    }
+
+    public void setPatch(String patch) {
+        this.patch = patch;
+    }
 }
