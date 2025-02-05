cf58f37b5b9a2d90134573a85432a0ff4a69f49c
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2025-02-05 14:27:08 +0200

    feat: add custom find and delete methods in postgres UserRepository

--- src/main/java/com/stergion/githubbackend/infrastructure/persistence/postgres/users/UserRepository.java
+++ src/main/java/com/stergion/githubbackend/infrastructure/persistence/postgres/users/UserRepository.java
@@ -1,10 +1,28 @@
 package com.stergion.githubbackend.infrastructure.persistence.postgres.users;
 
 import io.quarkus.hibernate.reactive.panache.PanacheRepositoryBase;
+import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
 
+import java.util.List;
 import java.util.UUID;
 
 @ApplicationScoped
 public class UserRepository implements PanacheRepositoryBase<User, UUID> {
+
+    public Uni<User> findByLogin(String login) {
+        return find("login", login).firstResult();
+    }
+
+    public Uni<List<User>> findByEmail(String email) {
+        return list("email", email);
+    }
+
+    public Uni<User> findByGitHubId(String githubId) {
+        return find("githubId", githubId).firstResult();
+    }
+
+    public Uni<Long> deleteByLogin(String login) {
+        return delete("login", login);
+    }
 }
