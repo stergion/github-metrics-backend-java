7cedbb81358fa70ec6067745b64890f74de65260
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2025-02-07 19:49:00 +0200

    feat: create sealed interface ContributionRepository

---
+++ src/main/java/com/stergion/githubbackend/infrastructure/persistence/postgres/contributions/repositories/ContributionRepository.java
@@ -0,0 +1,43 @@
+package com.stergion.githubbackend.infrastructure.persistence.postgres.contributions.repositories;
+
+import com.stergion.githubbackend.infrastructure.persistence.postgres.contributions.entities.Contribution;
+import io.quarkus.hibernate.reactive.panache.PanacheRepositoryBase;
+import io.smallrye.mutiny.Uni;
+
+import java.util.List;
+import java.util.UUID;
+
+public sealed interface ContributionRepository<T extends Contribution>
+        extends PanacheRepositoryBase<T, UUID>
+        permits CommitRepository, IssueRepository, IssueCommentRepository, PullRequestRepository,
+        PullRequestReviewRepository {
+
+    default Uni<T> findById(UUID id) {
+        return find("id", id).firstResult();
+    }
+
+    default Uni<T> findByGitHubId(String gitHubId) {
+        return find("githubId", gitHubId).firstResult();
+    }
+
+    default Uni<List<T>> findByUserId(UUID userId) {
+        return find("user.id", userId).list();
+    }
+
+    default Uni<List<T>> findByRepoId(UUID repositoryId) {
+        return find("repository.id", repositoryId).list();
+    }
+
+    default Uni<List<T>> findByUserIdAndRepoId(UUID userId, UUID repositoryId) {
+        return find("user.id = ?1 " + "and repository.id = ?2",
+                userId, repositoryId).list();
+    }
+
+    // Specialized deletion methods
+    default Uni<Long> delete(UUID userId) {
+        return delete("user.id", userId);
+    }
+
+    @Override
+    Uni<Long> deleteAll();
+}
