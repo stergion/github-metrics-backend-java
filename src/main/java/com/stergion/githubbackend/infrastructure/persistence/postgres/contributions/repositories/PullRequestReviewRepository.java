c92803160856580692f58992138ec42ab9f48202
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2025-02-07 19:51:26 +0200

    feat: create PullRequestReviewRepository implementing ContributionRepository

---
+++ src/main/java/com/stergion/githubbackend/infrastructure/persistence/postgres/contributions/repositories/PullRequestReviewRepository.java
@@ -0,0 +1,58 @@
+package com.stergion.githubbackend.infrastructure.persistence.postgres.contributions.repositories;
+
+import com.stergion.githubbackend.infrastructure.persistence.postgres.contributions.entities.PullRequestReview;
+import io.smallrye.mutiny.Uni;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.hibernate.reactive.mutiny.Mutiny;
+
+@ApplicationScoped
+public final class PullRequestReviewRepository
+        implements ContributionRepository<PullRequestReview> {
+
+    @Inject
+    Mutiny.SessionFactory sessionFactory;
+
+    @Override
+    public Uni<Long> deleteAll() {
+        // Delete associations
+        String deleteReviewsReviewCommentsQuery = "DELETE FROM PullRequestReviews_PullRequestReviewComments ";
+
+        // Delete associated entities
+        String deleteReviewCommentsQuery = "DELETE FROM PullRequestReviewComments";
+
+        // Delete Reviews
+        String deleteReviews = """
+                WITH ids AS MATERIALIZED (
+                    SELECT i.id
+                    FROM PullRequestReviews i
+                    JOIN Contributions c ON i.id = c.id
+                ),
+                delete_reviews AS (
+                    DELETE FROM PullRequestReviews
+                    WHERE id IN (SELECT id FROM ids)
+                ),
+                delete_contributions AS (
+                DELETE FROM Contributions
+                WHERE id IN (SELECT id FROM ids)
+                RETURNING id
+                )
+                SELECT count(*) FROM ids id
+                """;
+
+        return sessionFactory.withTransaction((session, tx) ->
+                        session.createNativeQuery(deleteReviewsReviewCommentsQuery)
+                               .executeUpdate()
+                               .flatMap(result ->
+                                               session.createNativeQuery(deleteReviewCommentsQuery)
+                                                      .executeUpdate()
+                                       )
+                               .flatMap(result ->
+                                               session.createNativeQuery(deleteReviews)
+                                                      .executeUpdate()
+                                       )
+                               .map(Long::valueOf)
+                                             );
+
+    }
+}
