a4513750b0f9b9229df709abba697e4a6721aa6e
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2025-02-11 23:20:31 +0200

    refactor: use repository adapter in repository service

--- src/main/java/com/stergion/githubbackend/domain/repositories/RepositoryService.java
+++ src/main/java/com/stergion/githubbackend/domain/repositories/RepositoryService.java
@@ -4,8 +4,7 @@ import com.stergion.githubbackend.common.batch.BatchProcessorConfig;
 import com.stergion.githubbackend.domain.utils.RepositoryIdCache;
 import com.stergion.githubbackend.domain.utils.types.NameWithOwner;
 import com.stergion.githubbackend.infrastructure.external.githubservice.service.RepositoryClient;
-import com.stergion.githubbackend.infrastructure.persistence.mongo.repositories.RepositoryEntity;
-import com.stergion.githubbackend.infrastructure.persistence.mongo.repositories.MongoRepositoryRepository;
+import com.stergion.githubbackend.infrastructure.persistence.mongo.repositories.MongoRepositoryRepositoryAdapter;
 import io.smallrye.mutiny.Multi;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
@@ -19,19 +18,20 @@ public class RepositoryService {
     @Inject
     RepositoryClient repoClient;
     @Inject
-    MongoRepositoryRepository repoRepository;
+    MongoRepositoryRepositoryAdapter repoRepository;
 
     @Inject
     RepositoryMapper repoMapper;
+
     @Inject
     RepositoryIdCache repositoryIdCache;
 
     public Repository getRepository(String owner, String name) {
-        RepositoryEntity repo = repoRepository.findByNameAndOwner(owner, name);
+        Repository repo = repoRepository.findByNameAndOwner(owner, name);
         if (repo == null) {
             throw new RepositoryNotFoundException(owner, name);
         }
-        return repoMapper.toDomain(repo);
+        return repo;
     }
 
     public ObjectId getRepositoryId(NameWithOwner repo) {
@@ -40,13 +40,13 @@ public class RepositoryService {
             return id;
         }
 
-        id = repoRepository.findByNameAndOwner(repo.owner(), repo.name()).id;
-        if (id == null) {
+        var repository = repoRepository.findByNameAndOwner(repo.owner(), repo.name());
+        if (repository == null) {
             throw new RepositoryNotFoundException(repo.owner(), repo.name());
         }
 
-        repositoryIdCache.put(repo, id);
-        return id;
+        repositoryIdCache.put(repo, repository.id());
+        return repository.id();
     }
 
     private Repository fetchRepository(String owner, String name) {
@@ -58,23 +58,14 @@ public class RepositoryService {
     }
 
     private Repository createRepository(Repository repo) {
-        var repoEntity = repoMapper.toEntity(repo);
-        repoRepository.save(repoEntity);
-
-        return repoMapper.toDomain(repoEntity);
+        repoRepository.persist(repo);
+        return repo;
     }
 
     private List<Repository> createRepositories(List<Repository> repo) {
-        var repoEntities = repo.stream()
-                           .map(repoMapper::toEntity)
-                           .distinct()
-                           .toList();
+        repoRepository.persist(repo);
 
-        repoRepository.save(repoEntities);
-
-        return repoEntities.stream()
-                    .map(repoMapper::toDomain)
-                    .toList();
+        return repo;
     }
 
     public Repository fetchAndCreateRepository(NameWithOwner nameWithOwner) {
@@ -116,9 +107,8 @@ public class RepositoryService {
     }
 
     public List<Repository> getRepositories(List<ObjectId> ids) {
-        List<RepositoryEntity> repos = repoRepository.findById(ids);
-        return repos.stream()
-                    .map(repoMapper::toDomain)
-                    .toList();
+        return repoRepository.findById(ids.stream()
+                                          .map(ObjectId::toHexString)
+                                          .toList());
     }
 }
