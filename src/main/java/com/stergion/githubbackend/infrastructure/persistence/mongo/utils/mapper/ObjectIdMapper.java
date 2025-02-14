e706315adb56d13c40056d2096e4ae1a677236c7
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2025-02-15 00:45:15 +0200

    fix: update ObjectIdMapper methods to correctly map between types

--- src/main/java/com/stergion/githubbackend/infrastructure/persistence/mongo/utils/mapper/ObjectIdMapper.java
+++ src/main/java/com/stergion/githubbackend/infrastructure/persistence/mongo/utils/mapper/ObjectIdMapper.java
@@ -4,27 +4,39 @@ import com.stergion.githubbackend.common.mappers.MapStructConfig;
 import org.bson.types.ObjectId;
 import org.mapstruct.Mapper;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Mapper(config = MapStructConfig.class)
 public interface ObjectIdMapper {
     default ObjectId toObjectId(String id) {
-        return new ObjectId(id);
+        return id == null ? null : new ObjectId(id);
     }
 
-    default List<ObjectId> toObjectId(List<String> id) {
-        return id.stream()
-                 .map(ObjectId::new)
-                 .toList();
+    default List<ObjectId> toObjectId(List<String> ids) {
+        if (ids == null || ids.isEmpty()) {
+            return new ArrayList<>();
+        }
+
+        return ids.stream()
+                  .filter(Objects::nonNull)
+                  .map(ObjectId::new)
+                  .toList();
     }
 
     default String toStringId(ObjectId id) {
-        return id.toHexString();
+        return id == null ? null : id.toHexString();
     }
 
-    default List<String> toStringId(List<ObjectId> id) {
-        return id.stream()
-                 .map(ObjectId::toHexString)
-                 .toList();
+    default List<String> toStringId(List<ObjectId> ids) {
+        if (ids == null || ids.isEmpty()) {
+            return new ArrayList<>();
+        }
+
+        return ids.stream()
+                  .filter(Objects::nonNull)
+                  .map(ObjectId::toHexString)
+                  .toList();
     }
 }
