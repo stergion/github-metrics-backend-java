2b0440ff47908f7597f7b575ad24aee704dd546d
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2025-02-14 16:00:35 +0200

    chore: remove unused class SearchStrategyFactory

--- src/main/java/com/stergion/githubbackend/domain/contirbutions/search/SearchStrategyFactory.java
+++
@@ -1,9 +0,0 @@
-package com.stergion.githubbackend.domain.contirbutions.search;
-
-public interface SearchStrategyFactory {
-    CommitSearchStrategy createCommitSearchStrategy();
-    IssueSearchStrategy createIssueSearchStrategy();
-    PullRequestSearchStrategy createPullRequestSearchStrategy();
-    PullRequestReviewSearchStrategy createPullRequestReviewSearchStrategy();
-    IssueCommentSearchStrategy createIssueCommentSearchStrategy();
-}
