1cefb592820cb4547a0a0d0f03ddef61ae6419ff
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2025-02-25 17:36:25 +0200

    feat: add helper methods for list mapping in mongo UserMapper

--- src/main/java/com/stergion/githubbackend/infrastructure/persistence/mongo/users/UserMapper.java
+++ src/main/java/com/stergion/githubbackend/infrastructure/persistence/mongo/users/UserMapper.java
@@ -7,12 +7,17 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Mapper(config = MapStructConfig.class, imports = {LocalDateTime.class}, uses =
         ObjectIdMapper.class)
 public interface UserMapper {
     User toDomain(UserEntity user);
 
+    List<User> toDomain(List<UserEntity> user);
+
     @Mapping(target = "updatedAt", expression = "java(LocalDateTime.now())")
     UserEntity toEntity(User user);
+
+    List<UserEntity> toEntity(List<User> user);
 }
