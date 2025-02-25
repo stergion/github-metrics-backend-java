b68eec878a910a0fb274ce7226caea3639b0c19e
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2025-02-25 17:35:38 +0200

    feat: add helper methods for list mapping in postgres UserMapper

--- src/main/java/com/stergion/githubbackend/infrastructure/persistence/postgres/users/UserMapper.java
+++ src/main/java/com/stergion/githubbackend/infrastructure/persistence/postgres/users/UserMapper.java
@@ -2,10 +2,15 @@ package com.stergion.githubbackend.infrastructure.persistence.postgres.users;
 
 import com.stergion.githubbackend.common.mappers.MapStructConfig;
 import com.stergion.githubbackend.domain.users.User;
+import com.stergion.githubbackend.infrastructure.persistence.postgres.repositories.RepositoryEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 
 @Mapper(config = MapStructConfig.class, imports = {LocalDateTime.class})
@@ -16,10 +21,14 @@ public interface UserMapper {
     @Mapping(target = "websiteURL", source = "websiteUrl")
     User toDomain(UserEntity user);
 
+    List<User> toDomain(List<UserEntity> user);
+
     @Mapping(target = "githubId", source = "github.id")
     @Mapping(target = "githubUrl", source = "github.url")
     @Mapping(target = "avatarUrl", source = "avatarURL")
     @Mapping(target = "websiteUrl", source = "websiteURL")
     @Mapping(target = "updatedAt", expression = "java(LocalDateTime.now())")
     UserEntity toEntity(User user);
+
+    List<UserEntity> toEntity(List<User> user);
 }
